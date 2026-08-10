2040036fd47005f6ded13cfd01aeae5a5b5fe1e8
charlie <ckd_qja@naver.com>
2026-08-11 05:10:09 +0900

    cern

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(new URL('.', import.meta.url).pathname, './'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+});
